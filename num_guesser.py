5cf435101d3eaf6c6954265c8ddf1a2f29ce1344
abbas <abbas.work07@gmail.com>
2023-08-04 18:50:17 +0100

    Update num_guesser.py

--- num_guesser.py
+++ num_guesser.py
@@ -13,12 +13,3 @@ while tries < 3:
     tries += 1
     if tries == 3:
         print(f"HAHA L BOZO the answer was {num} >:)) )")
-
-# guess != num:
-    # print('try again :)')
-    # guess = input('guess a number between 1 and 100: ')
-    # tries += 1
-    # if tries > 3:
-    #     print(f"you've run out of chances, the answer was {num}")
-    # break
-
